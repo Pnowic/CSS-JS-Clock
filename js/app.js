7cf7ea839778b74c6eb9b144677378b749f1eeba
Pnowic <nowickipiotrus@gmail.com>
2018-07-26 21:15:52 +0200

    timezone change functionality and AM/PM sign

--- js/app.js
+++ js/app.js
@@ -5,46 +5,50 @@ document.addEventListener("DOMContentLoaded", function(){
         hourHand = document.querySelector('.hour-hand'),
         allHands = document.querySelectorAll('.hand');
 
-    function setDate () {
 
-        const now = new Date();
+    const allTimezones = [...moment.tz.names()];
+    const timezonesSelect = $('.timezones');
 
-        const seconds = now.getSeconds();
+    allTimezones.forEach((timezone, key) => {
+
+        timezonesSelect.append($('<option>')
+            .attr({key : key,
+                value : timezone
+            })
+            .text(timezone));
+    });
+
+    let currentTimezone =  $('option:selected').val();
+
+    timezonesSelect.change(function () {
+        currentTimezone = $('option:selected').val();
+    });
+
+    function setDate (){
+
+        const now = moment();
+
+        const seconds = now.tz(`${currentTimezone}`).format('ss');
         const secondsDeg = ((seconds / 60) * 360) + 90;
         secondHand.style.transform = `rotate(${secondsDeg}deg)`;
 
-        const minutes = now.getMinutes();
+        const minutes = now.tz(`${currentTimezone}`).format('mm');
         const minutesDeg = ((minutes / 60) * 360) + 90;
         minHand.style.transform = `rotate(${minutesDeg}deg)`;
 
-        const hours = now.getHours();
+        const hours = now.tz(`${currentTimezone}`).format('HH');
         const hoursDeg = ((hours / 12) * 360) + 90;
         hourHand.style.transform = `rotate(${hoursDeg}deg)`;
 
+        const ampm = $('.ampm');
+
+        hours >= 12 ? ampm.text('PM') : ampm.text('AM');
+
         secondsDeg === 90 ? allHands.forEach (hand => hand.style.transition = 'none') : allHands.forEach(hand => hand.style.transition = '')
-
     }
-
-
 
     setInterval(setDate, 1000);
 
     setDate();
-
-
-
-    const allTimezones = ['Your local time', ...moment.tz.names()];
-    console.log(allTimezones);
-
-    const timezones = $('.timezones');
-
-    allTimezones.forEach((timezone, key) => {
-
-        timezones.append($('<option></option>')
-            .attr('key', key)
-            .text(timezone));
-
-    });
-
 
 });
