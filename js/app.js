a875fc534dc66157f747233857dd9982d482018c
Pnowic <nowickipiotrus@gmail.com>
2018-07-27 15:12:27 +0200

    add local time as default

--- js/app.js
+++ js/app.js
@@ -6,7 +6,7 @@ document.addEventListener("DOMContentLoaded", function(){
         allHands = document.querySelectorAll('.hand');
 
 
-    const allTimezones = [...moment.tz.names()];
+    const allTimezones = ['Your local time', ...moment.tz.names()];
     const timezonesSelect = $('.timezones');
 
     allTimezones.forEach((timezone, key) => {
@@ -28,15 +28,18 @@ document.addEventListener("DOMContentLoaded", function(){
 
         const now = moment();
 
-        const seconds = now.tz(`${currentTimezone}`).format('ss');
+        let seconds = now.format('ss');
+        currentTimezone === 'Your local time' ? seconds = now.format('ss') : now.tz(`${currentTimezone}`).format('ss');
         const secondsDeg = ((seconds / 60) * 360) + 90;
         secondHand.style.transform = `rotate(${secondsDeg}deg)`;
 
-        const minutes = now.tz(`${currentTimezone}`).format('mm');
+        let minutes = now.format('mm');
+        currentTimezone === 'Your local time' ? minutes = now.format('mm') : now.tz(`${currentTimezone}`).format('mm');
         const minutesDeg = ((minutes / 60) * 360) + 90;
         minHand.style.transform = `rotate(${minutesDeg}deg)`;
 
-        const hours = now.tz(`${currentTimezone}`).format('HH');
+        let hours = now.format('HH');
+        currentTimezone === 'Your local time' ? hours = now.format('HH') : now.tz(`${currentTimezone}`).format('HH');
         const hoursDeg = ((hours / 12) * 360) + 90;
         hourHand.style.transform = `rotate(${hoursDeg}deg)`;
 
@@ -45,6 +48,8 @@ document.addEventListener("DOMContentLoaded", function(){
         hours >= 12 ? ampm.text('PM') : ampm.text('AM');
 
         secondsDeg === 90 ? allHands.forEach (hand => hand.style.transition = 'none') : allHands.forEach(hand => hand.style.transition = '')
+
+
     }
 
     setInterval(setDate, 1000);
